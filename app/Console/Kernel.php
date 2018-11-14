73ed6f9be635fa87c8886b2e8904e48b64009df9
mr_dreek <nikitka2904@gmail.com>
2018-11-14 10:34:37 +0300

    version 1.11.3 cron setup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call('App\Http\Controllers\ServicesController@loadServiceFromApi')
-            ->everyMinute();
+            ->hourly();
     }
 
     /**
